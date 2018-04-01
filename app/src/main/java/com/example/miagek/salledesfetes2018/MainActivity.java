8d3a30fab23213120a08c2934950e667481c7d57
adil boukili <tmbtp.boukili@gmail.com>
2018-04-01 22:07:50 +0000

    hadi awaaal version dial l app SalleDesFetes2018

---
+++ app/src/main/java/com/example/miagek/salledesfetes2018/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.miagek.salledesfetes2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
